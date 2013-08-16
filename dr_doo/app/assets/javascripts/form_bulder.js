f26c59d3258f99c122e4568d34e089a804aad0cd
Apprentice <apprentice@devbootcamp.com>
2013-08-15 23:00:15 -0700

    here

---
+++ dr_doo/app/assets/javascripts/form_bulder.js
@@ -0,0 +1,18 @@
+$(function() {
+  // $('.field_name').draggable();
+  // var $elem = $('')
+  // $('.name_field').draggable();
+
+// $(".gridster").gridster({
+// 	widget_selector: '> input'
+//         widget_margins: [1, 1],
+//         widget_base_dimensions: [1, 1]
+//     });
+
+   var gridster = $(".gridster li").gridster().data('gridster');
+
+// gridster.add_widget('<li class="new">The HTML of the widget...</li>', 2, 1);
+
+
+
+});
