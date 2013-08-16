6325846f42927122f01eb8b564dca7a49d53d4f4
Apprentice <apprentice@devbootcamp.com>
2013-08-15 18:34:12 -0700

    all associations and basic models now woking with a basic seed file

--- dr_doo/app/models/formfields.rb
+++
@@ -1,5 +0,0 @@
-class Formfields < ActiveRecord::Base
-	belongs_to :form
-	belongs_to :field
-  # attr_accessible :title, :body
-end
--- dr_doo/db/migrate/20130815230100_create_fields.rb
+++ dr_doo/db/migrate/20130815230100_create_fields.rb
@@ -2,7 +2,7 @@ class CreateFields < ActiveRecord::Migration
   def change
     create_table :fields do |t|
     	t.string :default_label
-    	t.string :type
+    	t.string :field_type
 
       t.timestamps
     end
